4294c56c92f1a51fff83b14f837475dbb050c9bb
yasik <yasikchoudhary20@gmail.com>
2023-08-23 21:45:17 +0530

    v2

---
+++ Weather-App-Api/index.js
@@ -0,0 +1,39 @@
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests");
+
+const homeFile = fs.readFileSync("./home.html", "utf-8");
+const replaceVal = (tempVal, orgVal) => {
+  let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+  temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+  temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+  temperature = temperature.replace("{%location%}", orgVal.name);
+  temperature = temperature.replace("{%country%}", orgVal.sys.country);
+  temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+
+  return temperature;
+};
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    requests(
+      `https://api.openweathermap.org/data/2.5/weather?q=Karachi&appid=0e9b81fb9bf8660e92b548a181cc5e0e`
+    )
+      .on("data", (chunk) => {
+        const objdata = JSON.parse(chunk);
+        const arrData = [objdata];
+        const realTimeData = arrData
+          .map((val) => replaceVal(homeFile, val))
+          .join("");
+        res.write(realTimeData);
+      })
+      .on("end", (err) => {
+        if (err) return console.log("connection closed due to errors", err);
+        res.end();
+      });
+  }
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server is Running `);
+});
